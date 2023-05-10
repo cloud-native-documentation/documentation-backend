c22dcd5e8faba152b9065a48d63c43e6bd9eaea7
y-c-wang <ycwang.cs@gmail.com>
2023-05-10 23:19:16 +0800

    allow all cors origin

--- DocManageSystem/DocManageSystem/settings.py
+++ DocManageSystem/DocManageSystem/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'usermanagement'
-    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'DocManageSystem.urls'
 
