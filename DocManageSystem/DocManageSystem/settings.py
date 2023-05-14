35fc52a86a18ea2c42082767b9127a2091c3b2ff
roylin506 <roylin506@gmail.com>
2023-05-14 12:47:44 +0800

    add routing

--- DocManageSystem/DocManageSystem/settings.py
+++ DocManageSystem/DocManageSystem/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'usermanagement'
+    'usermanagement', 
+    'documentmanagement'
 ]
 
 MIDDLEWARE = [
--- DocManageSystem/DocManageSystem/urls.py
+++ DocManageSystem/DocManageSystem/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('user/', include('usermanagement.urls'))
+    path('user/', include('usermanagement.urls')),
+    path('document/', include('documentmanagement.urls'))
 ]
