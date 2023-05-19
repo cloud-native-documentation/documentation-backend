949f54b172e663e67081330a2ffa27070d77fdf8
roylin506 <roylin506@gmail.com>
2023-05-20 00:45:52 +0800

    modify urlpatterns

--- DocManageSystem/documentmanagement/url.py
+++ DocManageSystem/documentmanagement/url.py
@@ -4,7 +4,6 @@ urlpatterns = [
  path('create_project', project_create), 
  path('delete_project', project_delete), 
  path('project_list', project_list), 
- path('permission_list', project_permission_list), 
  path('create_directory', dir_create), 
  path('delete_directory', dir_delete),
  path('list', doc_list), 
