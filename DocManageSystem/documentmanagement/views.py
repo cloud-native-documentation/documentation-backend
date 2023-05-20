158cba74f6a14cc2ed29857506d29bcec3b405bd
roylin506 <roylin506@gmail.com>
2023-05-21 01:18:39 +0800

    finish project function

--- DocManageSystem/documentmanagement/views.py
+++ DocManageSystem/documentmanagement/views.py
@@ -2,40 +2,63 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Doc, Dir, Project
-
+import os
+root_dir = './files/'
+os.system(f'mkdir {root_dir}')
 
 @api_view(['POST'])
 def project_create(request):
-    projects = Project.objects.all()
-    return Response()
+    projectname=request.data['projectname']
+    project = Project.objects.filter(projectname=projectname)
+    
+    if project is not []:
+        data = {"status": "fail"}
+        return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+    
+    department = request.META.get('user').department
+    instance = Project(projectname=projectname, department=department)
+    instance.save()
+    
+    data = {"status": "success"}
+    return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def project_delete(request):
-    projects = Project.objects.all()
-    return Response()
+    projectname=request.data['projectname']
+    projects = Project.objects.filter(projectname=projectname)
+    
+    if projects == []:
+        data = {"status": "fail"}
+        return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+        
+    projects[0].delete()
+    
+    data = {"status": "success"}
+    return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def project_list(request):
     projects = Project.objects.all()
-    return Response()
-
-
-# @api_view(['GET'])
-# def project_permission_list(request):
-#     return Response()
+    project_list = [project.projectname for project in projects]
+    data = {"status": "success", "projectlist": project_list}
+    return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def dir_create(request):
-    dirs = Dir.objects.all()
-    return Response()
+    """get user name"""
+    projectpath = f'{root_dir}{request.data["project"]}'
+    dirpath = f'{projectpath}/{request.data["directoryname"]}'
+    str = os.popen(f'mkdir {projectpath}')
+    os.popen(f'mkdir {dirpath}')
+    return Response({"status": "fail"}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def dir_delete(request):
-    dirs = Dir.objects.all()
+    
     return Response()
 
 
