05a7fb3333cc7b1a765ca85fd9558e57732902a5
roylin506 <roylin506@gmail.com>
2023-05-20 10:51:08 +0800

    remove serializers

--- DocManageSystem/documentmanagement/views.py
+++ DocManageSystem/documentmanagement/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Doc, Dir, Project
-from .serializers import DocSerializer, DirSerializer, ProjectSerializer
 
 
 @api_view(['POST'])
